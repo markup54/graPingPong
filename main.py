b1f45cab6203696c7252a3f52bf25ae849927c8e
markup54 <marzenakupka@tlen.pl>
2025-05-05 09:13:39 +0200

    ruch piłki

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 running = True
 
+x_ball = 10
+y_ball = 10
+move_ball = 3
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
            running = False
-
+    x_ball = x_ball + move_ball
+    y_ball = y_ball + move_ball
     screen.fill("green")
-    pygame.draw.circle(screen,"white",(30,60),10)
+    pygame.draw.circle(screen,"white",(x_ball,y_ball),10)
     pygame.display.flip()
     clock.tick(60)
+
+pygame.quit()
 
