6e1c8ef7500bfc9d59ec570f804ae126c1b26ccc
markup54 <marzenakupka@tlen.pl>
2025-05-05 08:46:51 +0200

    pusty projekt

---
+++ main.py
@@ -0,0 +1,15 @@
+a =10
+if a%2 == 0:
+    print("liczba parzysta")
+else:
+    print(a, "nieparzyste")
+
+for i in range(2,10,2):
+    print(i)
+
+while a<30:
+    a=a+10
+    print("kolejna iteracja")
+print("wyszlo z pętli")
+
+print(print.__doc__)
