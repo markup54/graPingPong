af8be88859bb79cc80e30affb1546cf57ca52fff
markup54 <marzenakupka@tlen.pl>
2025-05-05 09:21:12 +0200

    pilka nie ucieka

--- main.py
+++ main.py
@@ -10,14 +10,25 @@ running = True
 
 x_ball = 10
 y_ball = 10
-move_ball = 3
+move_ball_x = 3
+move_ball_y = 3
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
            running = False
-    x_ball = x_ball + move_ball
-    y_ball = y_ball + move_ball
+    x_ball = x_ball + move_ball_x
+    y_ball = y_ball + move_ball_y
+    if y_ball > screen.get_height():
+        move_ball_y = -move_ball_y
+
+    if y_ball< 0:
+        move_ball_y = -move_ball_y
+
+    if x_ball>screen.get_width():
+        move_ball_x = - move_ball_x
+    if x_ball<0:
+        move_ball_x = - move_ball_x
     screen.fill("green")
     pygame.draw.circle(screen,"white",(x_ball,y_ball),10)
     pygame.display.flip()
